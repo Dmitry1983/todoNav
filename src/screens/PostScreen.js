d5eb0b963dca2e5645f34aa2dd188a7bae971e2d
Dmitry1983 <barbash.dv@gmail.com>
2020-05-18 12:41:02 +0300

    postScreen

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -36,7 +36,7 @@ export const PostScreen = ({ navigation, route }) => {
     React.useEffect(() => {
         //navigation.setOptions({ title: post.text })
         navigation.setOptions({ title: new Date(post.date).toLocaleDateString() })
-        console.log('Open postScreen ' + post.text)
+        console.log('Open postScreen' + post.text)
     })
 
     const { view, text, image, buttonView } = styles
