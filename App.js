1b053314e41a7569754e482a7598771ee02bd497
diBasch <barbash.dv@yandex.com>
2020-06-15 11:26:06 +0300

    airbnb

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,20 +6,19 @@ import {
   View,
   Text,
   StatusBar,
-  Button
-} from 'react-native'
-import { Provider } from 'react-redux'
-import store from './src/store'
-import { bootstap } from './src/bootstrap';
-import AppNavigator from './src/navigation/AppNavigation'
+  Button,
+} from 'react-native';
+import {Provider} from 'react-redux';
+import store from './src/store';
+import {bootstap} from './src/bootstrap';
+import AppNavigator from './src/navigation/AppNavigation';
 
 const App = () => {
-
   return (
-    <Provider store={store} >
+    <Provider store={store}>
       <AppNavigator />
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,50 +1,55 @@
-import React, { useEffect } from 'react'
-import { Text, View, StyleSheet, Button, FlatList, SafeAreaView } from 'react-native'
+import React, {useEffect} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  FlatList,
+  SafeAreaView,
+} from 'react-native';
 //import { DATA } from '../data'
-import {useDispatch, useSelector} from 'react-redux'
-import { Post } from '../components/Post'
-import { THEME } from '../theme'
-import { loadPosts } from '../store/actions/postactions'
+import {useDispatch, useSelector} from 'react-redux';
+import {Post} from '../components/Post';
+import {THEME} from '../theme';
+import {loadPosts} from '../store/actions/postactions';
 
-export const MainScreen = ({ navigation }) => {
-    const { view, flatList } = styles
+export const MainScreen = ({navigation}) => {
+  const {view, flatList} = styles;
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        //navigation.setOptions({ title: 'Updated!' })
-        console.log('Open mainScreen ')
-        dispatch(loadPosts())
-    },[dispatch])
+  useEffect(() => {
+    //navigation.setOptions({ title: 'Updated!' })
+    console.log('Open mainScreen ');
+    dispatch(loadPosts());
+  }, [dispatch]);
 
-    const openPostHandler = (post) => {
-        navigation.navigate('postScreen', { post: post })
-    }
+  const openPostHandler = (post) => {
+    navigation.navigate('postScreen', {post: post});
+  };
 
-    const allPosts = useSelector(state=>state.post.allPosts)
+  const allPosts = useSelector((state) => state.post.allPosts);
 
-
-    return (
-        <SafeAreaView style={view}>
-            <FlatList
-                style={flatList}
-                data={allPosts}
-                //keyExtractor={post => post.id.toString()}
-               renderItem={({ item }) => <Post post={item} onOpen={openPostHandler} />}
-            />
-        </SafeAreaView>
-    )
-}
-
+  return (
+    <SafeAreaView style={view}>
+      <FlatList
+        style={flatList}
+        data={allPosts}
+        //keyExtractor={post => post.id.toString()}
+        renderItem={({item}) => <Post post={item} onOpen={openPostHandler} />}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    view: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: THEME.COLOR.BLACK
-    },
-    flatList: {
-        width: '100%'
-    }
-})
+  view: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: THEME.COLOR.BLACK,
+  },
+  flatList: {
+    width: '100%',
+  },
+});
